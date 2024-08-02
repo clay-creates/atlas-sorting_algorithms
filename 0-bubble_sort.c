336f48574bb27985918415fe6454c4bd063afdcf
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-08-02 02:06:09 +0000

    for j for loop in bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,7 +18,7 @@ void bubble_sort(int *array, size_t size)
 
     for (i = 0; i < size; i++)
     {
-        for (j = 0; j < size - 1; j++)
+        for (j = 0; j < size - i - 1; j++)
         {
             temp = array[j];
             array[j] = array[j + 1];
