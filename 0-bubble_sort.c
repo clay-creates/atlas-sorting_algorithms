c8c325ac744a8a5792b992c46e703851866eeeaa
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-08-02 01:39:06 +0000

    testing bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,6 +10,7 @@ void bubble_sort(int *array, size_t size)
 {
     size_t i, j;
     int temp;
+    int swapped;
 
     if (array == NULL || size < 20)
     {
@@ -18,12 +19,18 @@ void bubble_sort(int *array, size_t size)
 
     for (i = 0; i < size; i++)
     {
+        swapped = 0;
         for (j = 0; j < size - 1; j++)
         {
             temp = array[j];
             array[j] = array[j + 1];
             array[j + 1] = temp;
             print_array(array, size);
+            swapped = 1;
         }
+    }
+    if (!swapped)
+    {
+        break;
     }
 }
