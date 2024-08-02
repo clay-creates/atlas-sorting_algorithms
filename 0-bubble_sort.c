458856bf05a6ea2d23ef46ac6048b01944dab265
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-08-02 01:32:49 +0000

    make bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of ints ascending w/ Bubble sort
+ * @array: Array to be sorted
+ * @size: Size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+    int temp;
+
+    if (array == NULL || size < 20)
+    {
+        return;
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size - 1; j++)
+        {
+            temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+            print_array(array, size);
+        }
+    }
+}
